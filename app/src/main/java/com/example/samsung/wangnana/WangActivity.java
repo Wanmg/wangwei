46a79ba367a4b9bce393a0f056fb21decb44f1d0
Wanmg <547370042@qq.com>
2017-04-14 15:36:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/samsung/wangnana/WangActivity.java
@@ -0,0 +1,13 @@
+package com.example.samsung.wangnana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wang);
+    }
+}
